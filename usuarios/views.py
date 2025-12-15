307f44381037e1033ac3655fd5abeecd596d678c
SebasGit18 <sebitaswz3@gmail.com>
2025-12-15 17:09:36 -0500

    style sebastian: se configuro el diseño del login

--- usuarios/views.py
+++ usuarios/views.py
@@ -4,19 +4,9 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 
-def login_view(request):
-    if request.method == 'POST':
-        usuario = request.POST.get('usuario')
-        clave = request.POST.get('clave')
+from django.contrib.auth.views import LoginView
 
-        user = authenticate(request, username=usuario, password=clave)
+class Logueo(LoginView):
+    template_name = "components/forloggin.html"
+    redirect_authenticated_user = True
 
-        if user is not None:
-            login(request, user)
-            return redirect('login')  # cámbialo luego a dashboard
-
-        return render(request, 'usuarios/login.html', {
-            'error': 'Usuario o contraseña incorrectos'
-        })
-
-    return render(request, 'usuarios/login.html')

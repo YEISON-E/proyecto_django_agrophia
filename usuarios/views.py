a7f6bfd7ddc09339a50273929d5acfeb32000854
Yeison Quintero <qyeisonestiven@gmail.com>
2026-02-04 09:26:54 -0500

    modifico el login

--- usuarios/views.py
+++ usuarios/views.py
@@ -7,16 +7,12 @@ from django.contrib.auth import authenticate, login
 def login_view(request):
     if request.method == 'POST':
         usuario = request.POST.get('usuario')
-        clave = request.POST.get('clave')
-
-        user = authenticate(request, username=usuario, password=clave)
-
-        if user is not None:
-            login(request, user)
-            return redirect('login')  # cámbialo luego a dashboard
-
-        return render(request, 'usuarios/login.html', {
-            'error': 'Usuario o contraseña incorrectos'
-        })
+        password = request.POST.get('password')
+        # Aquí luego conectamos autenticación real
+        print(usuario, password)
 
     return render(request, 'usuarios/login.html')
+    from django.shortcuts import redirect
+
+def home(request):
+    return redirect('login')
